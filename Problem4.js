ea936480ce85f783f44ec2fa9756490e70e9a480
Vivek <vivek.15@mountblue.tech>
2021-02-09 00:26:35 +0530

    Created fuction listOfCarYear which return list of Years of cars present in inventory

---
+++ Problem4.js
@@ -0,0 +1,9 @@
+function listOfCarYears(inventory) {
+    let carYears = [];
+    for (let i=0; i < inventory.length; i++) {
+        carYears.push(inventory[i].car_year);
+    }
+    return carYears;
+}
+
+module.exports = listOfCarYears;
