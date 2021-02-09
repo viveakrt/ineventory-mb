4863d5fe62469c509049b931dcf9123a015d40a3
Vivek <vivek.15@mountblue.tech>
2021-02-09 16:51:41 +0530

    resolve the issue of array passing

--- problem1.js
+++ problem1.js
@@ -5,13 +5,14 @@
  * @param {number} id Id of car we required the detail
  */
 function callById(inventory, id) {
-    let car_model=[]
+    const car_model=[]
     if(inventory.length===0 || id === undefined){
         return car_model
     }
     for (let i=0; i < inventory.length; i++) {
-        car_model = inventory[i];
-        if (car_model.id === id) {
+        cars = inventory[i];
+        if (cars.id === id) {
+            car_model.push(inventory[i])
             return car_model;
         }
     }
