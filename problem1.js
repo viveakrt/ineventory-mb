f69d3c2695beefc744add832b1d37a0b888cffe8
Vivek <vivek.15@mountblue.tech>
2021-02-09 16:28:59 +0530

    resolve the issue of empty array passing

--- problem1.js
+++ problem1.js
@@ -5,12 +5,17 @@
  * @param {number} id Id of car we required the detail
  */
 function callById(inventory, id) {
+    let car_model={}
+    if(inventory.length===0 || id === undefined){
+        return car_model
+    }
     for (let i=0; i < inventory.length; i++) {
-        let car_model = inventory[i];
+        car_model = inventory[i];
         if (car_model.id === id) {
             return car_model;
         }
     }
+    return car_model
 }
 
 module.exports = callById;
